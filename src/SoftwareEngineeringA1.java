7229031f1d5b84d1de1edf135bbce7fd0cf333d5
jeremy23newman <jeremy23newman@gmail.com>
2024-09-03 19:48:20 -0400

    updating failing code

--- src/SoftwareEngineeringA1.java
+++ src/SoftwareEngineeringA1.java
@@ -2,7 +2,6 @@ package softwareEngineeringA1;
 
 public class SoftwareEngineeringA1 {
 
-	public static void main(String[] args) {
 	public static void (String[] args) {
 int numberOfIterations = 20;
 		
