1ff6a55154aa6d6a551fd3779bad705d22c4a432
jeremy23newman <jeremy23newman@gmail.com>
2024-09-03 19:32:55 -0400

    pushing incorrect code

---
+++ src/SoftwareEngineeringA1.java
@@ -0,0 +1,15 @@
+package softwareEngineeringA1;
+
+public class SoftwareEngineeringA1 {
+
+	public static void main(String[] args) {
+int numberOfIterations = 20;
+		
+		for(int i = 1; i <= numberOfIterations; i++) {
+			System.out.println("Here is " + i);
+			
+		}
+
+	}
+
+}
