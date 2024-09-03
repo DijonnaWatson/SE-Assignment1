498cab6335231bd2831785fcaf2dd8fc6cd80417
jeremy23newman <jeremy23newman@gmail.com>
2024-09-03 19:46:26 -0400

    submitting status fail code

--- src/SoftwareEngineeringA1.java
+++ src/SoftwareEngineeringA1.java
@@ -3,6 +3,7 @@ package softwareEngineeringA1;
 public class SoftwareEngineeringA1 {
 
 	public static void main(String[] args) {
+	public static void (String[] args) {
 int numberOfIterations = 20;
 		
 		for(int i = 1; i <= numberOfIterations; i++) {
