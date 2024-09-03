bd6dcb874822085cf304a950beb9bb07032c3c08
jeremy23newman <jeremy23newman@gmail.com>
2024-09-03 19:55:50 -0400

    fixing broken code

--- src/SoftwareEngineeringA1.java
+++ src/SoftwareEngineeringA1.java
@@ -2,8 +2,8 @@ package softwareEngineeringA1;
 
 public class SoftwareEngineeringA1 {
 
-	public static void (String[] args) {
-int numberOfIterations = 20;
+	public static void main (String[] args) {
+		int numberOfIterations = 20;
 		
 		for(int i = 1; i <= numberOfIterations; i++) {
 			System.out.println("Here is " + i);
