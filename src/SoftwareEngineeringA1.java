15d9bf63a881c7b2222e490c10e1b2c3babeb904
jeremy23newman <jeremy23newman@gmail.com>
2024-09-03 20:17:33 -0400

    updating indentation

--- src/SoftwareEngineeringA1.java
+++ src/SoftwareEngineeringA1.java
@@ -1,15 +1,11 @@
 package softwareEngineeringA1;
 
 public class SoftwareEngineeringA1 {
+  public static void main(String[] args) {
+    int numberOfIterations = 20;
 
-	public static void main(String[] args) {
-		int numberOfIterations = 20;
-
-		for (int i = 1; i <= numberOfIterations; i++) {
-			System.out.println("Here is " + i);
-
-		}
-
-	}
-
+    for (int i = 1; i <= numberOfIterations; i++) {
+      System.out.println("Here is " + i);
+    }
+  }
 }
