d77ab1594ddf768282acd1ae28da1f80676a137b
jeremy23newman <jeremy23newman@gmail.com>
2024-09-14 17:11:06 -0400

    attempting to add fibonacci api

---
+++ src/FibonacciInterface2.java
@@ -0,0 +1,15 @@
+package fibonacci.se;
+
+public interface FibonacciInterface {
+	
+	/**
+	 * Computes fibonacci sequence up to specified term.
+	 * 
+	 * @param numberOfTerms, number of terms in the fibonacci sequence.
+	 * @param firstTerm, first term inputed by user.
+	 * @param secondTerm, second term inputed by user.
+	 * @return, returns the final term in the sequence
+	 */
+	public int Fibonacci(int numberOfTerms, int firstTerm, int secondTerm);
+
+}
---
+++ src/FibonacciMethod2.java
@@ -0,0 +1,32 @@
+package fibonacci.se;
+
+public class FibonacciMethod implements FibonacciInterface {
+
+	
+	/**
+	 * Computes fibonacci sequence up to specified term.
+	 * 
+	 * @param numberOfTerms, number of terms in the fibonacci sequence.
+	 * @param firstTerm, first term inputed by user.
+	 * @param secondTerm, second term inputed by user.
+	 * @return, returns the final term in the sequence
+	 */
+	public int Fibonacci(int numberOfTerms, int firstTerm, int secondTerm) {
+		if (numberOfTerms < 3) 
+			throw new IllegalArgumentException("The number of terms cannot be less than three.");
+		else if(numberOfTerms > 45)
+			throw new IllegalArgumentException("The number of terms cannot be greater than forty-five.");
+		
+		int storedTerm = 0;
+		for(int i = 2; i < numberOfTerms;i++) {
+			storedTerm = firstTerm + secondTerm;
+			firstTerm = secondTerm;
+			secondTerm = storedTerm;
+			
+			
+		}
+		return storedTerm;
+	}
+	
+
+}
