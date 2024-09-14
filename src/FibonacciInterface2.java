cdd6126b5cff068364ee7717555a6209bb4e7507
jeremy23newman <jeremy23newman@gmail.com>
2024-09-14 17:13:19 -0400

    updating format of interface

--- src/FibonacciInterface2.java
+++ src/FibonacciInterface2.java
@@ -1,15 +1,13 @@
 package fibonacci.se;
 
 public interface FibonacciInterface {
-	
-	/**
-	 * Computes fibonacci sequence up to specified term.
-	 * 
-	 * @param numberOfTerms, number of terms in the fibonacci sequence.
-	 * @param firstTerm, first term inputed by user.
-	 * @param secondTerm, second term inputed by user.
-	 * @return, returns the final term in the sequence
-	 */
-	public int Fibonacci(int numberOfTerms, int firstTerm, int secondTerm);
-
+  /**
+   * Computes fibonacci sequence up to specified term.
+   *
+   * @param numberOfTerms, number of terms in the fibonacci sequence.
+   * @param firstTerm, first term inputed by user.
+   * @param secondTerm, second term inputed by user.
+   * @return, returns the final term in the sequence
+   */
+  public int Fibonacci(int numberOfTerms, int firstTerm, int secondTerm);
 }
