13d1760ad15a6b83e8a808921f743a33e1605c7a
jeremy23newman <jeremy23newman@gmail.com>
2024-09-14 17:15:30 -0400

    updating format of prototype

--- src/FibonacciPrototype2.java
+++ src/FibonacciPrototype2.java
@@ -2,21 +2,19 @@ package fibonacci.se;
 import java.util.Scanner;
 
 public class FibonacciPrototype {
+  public static void main(String[] args) {
+    Scanner userInput = new Scanner(System.in);
 
-	public static  void main(String[] args) {
-		
-		Scanner userInput = new Scanner(System.in);
-		
-		System.out.println("Enter Fibonacci Sequence Terms(between 3 - 45): "); 
-		
-		int numberOfTerms = userInput.nextInt(); //scanner input for number of terms in fibonacci
-		
-		FibonacciMethod fibMethod = new FibonacciMethod();
-		
-        int result = fibMethod.Fibonacci(numberOfTerms, 1, 2);
-        
-        System.out.println("The final term in the fibonacci sequence starting "
-        		+ "with first term: 1 and second term: 2 is " + result);
-	}
+    System.out.println("Enter Fibonacci Sequence Terms(between 3 - 45): ");
 
+    int numberOfTerms =
+        userInput.nextInt(); // scanner input for number of terms in fibonacci
+
+    FibonacciMethod fibMethod = new FibonacciMethod();
+
+    int result = fibMethod.Fibonacci(numberOfTerms, 1, 2);
+
+    System.out.println("The final term in the fibonacci sequence starting "
+        + "with first term: 1 and second term: 2 is " + result);
+  }
 }
