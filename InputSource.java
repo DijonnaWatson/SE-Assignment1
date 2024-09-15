edaa71183675f5cf61aba63435cab2270875562b
DijonnaWatson <167108324+DijonnaWatson@users.noreply.github.com>
2024-09-15 13:34:19 -0400

    Delete InputSource.java

--- InputSource.java
+++
@@ -1,5 +0,0 @@
-public interface InputSource {
-    int readFromFile();   // Method to read from file
-    int readFromConsole();  // Method to read from console
-    void setDelimiter(String delimiter);  // Set the delimiter for output
-}
