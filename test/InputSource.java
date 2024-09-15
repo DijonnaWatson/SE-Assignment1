f94a13c9d5cfd07cf809ca845b9a9cf46cbeb333
DijonnaWatson <167108324+DijonnaWatson@users.noreply.github.com>
2024-09-15 13:39:03 -0400

    Update InputSource.java

--- test/InputSource.java
+++ test/InputSource.java
@@ -1,6 +1,5 @@
-
 public interface InputSource {
-	int readFromFile();   // Method to read from file
-    int readFromConsole();  // Method to read from console
-	void setDelimiter(String delimiter);  // Set the delimiter for output
+  int readFromFile(); // Method to read from file
+  int readFromConsole(); // Method to read from console
+  void setDelimiter(String delimiter); // Set the delimiter for output
 }
