0b25b3e793ef7add788ebf7a36b5826ca37f38ff
DijonnaWatson <167108324+DijonnaWatson@users.noreply.github.com>
2024-09-15 13:17:59 -0400

    Update FactorFinder.java
    fixing indentations

--- FactorFinder.java
+++ FactorFinder.java
@@ -1,17 +1,16 @@
-//Main IF
 public interface FactorFinder {
 	/**
-     * Calculates the factors of the given number.
-     *
-     * @param number The number to find factors for.
-     * @return An array of factors of the given number.
-     */
-    int[] getFactors(int number);
+	 * Calculates the factors of the given number.
+	 *
+	 * @param number The number to find factors for.
+	 * @return An array of factors of the given number.
+	 */
+	int[] getFactors(int number);
 
-    /**
-     * Sets the delimiter to be used in the output.
-     *
-     * @param delimiter The delimiter to use between factors.
-     */
-    void setDelimiter(String delimiter);
+	/**
+	 * Sets the delimiter to be used in the output.
+	 *
+	 * @param delimiter The delimiter to use between factors.
+	 */
+	void setDelimiter(String delimiter);
 }
