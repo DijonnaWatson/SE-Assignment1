cb08db17400da9c2a950f67ddf6d6cfc9d1b3b3d
DijonnaWatson <167108324+DijonnaWatson@users.noreply.github.com>
2024-09-15 13:27:24 -0400

    Update InputSource.java
    fixing indentation again

--- InputSource.java
+++ InputSource.java
@@ -1,5 +1,5 @@
 public interface InputSource {
-	int readFromFile();   // Method to read from file
+    int readFromFile();   // Method to read from file
     int readFromConsole();  // Method to read from console
-	void setDelimiter(String delimiter);  // Set the delimiter for output
+    void setDelimiter(String delimiter);  // Set the delimiter for output
 }
