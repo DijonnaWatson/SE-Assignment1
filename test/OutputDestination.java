d253a85de5c22b941d5f847a3de1b68426b78ba0
DijonnaWatson <167108324+DijonnaWatson@users.noreply.github.com>
2024-09-15 13:48:24 -0400

    Update OutputDestination.java

--- test/OutputDestination.java
+++ test/OutputDestination.java
@@ -1,4 +1,3 @@
-package com.example.default;
 public interface OutputDestination {
   void outputFactors(int[] factors,
       String outputDestination); // Output factors (could be file or console)
