5ad3d92db845ebb74c5dcd347a1b4d99ad9dfff1
ViceRaven <167108271+ViceRaven@users.noreply.github.com>
2024-09-15 16:17:23 -0400

    Update Factorials.java

--- src/Factorials.java
+++ src/Factorials.java
@@ -4,26 +4,26 @@ import java.util.Scanner;
 
 public class Factorials {
 
-    public static void main(String[] args) {
-        Scanner keyboard = new Scanner(System.in);
+  public static void main(String[] args) {
+    Scanner keyboard = new Scanner(System.in);
 
         // Get user input
-        System.out.println("Compute factorial of a number between 1 and 20: ");
-        int number = keyboard.nextInt();
+    System.out.println("Compute factorial of a number between 1 and 20: ");
+    int number = keyboard.nextInt();
         
         // Check for valid input
-        while (number < 1 || number > 20) {
-            System.out.println("Your number is out of range. Please enter a number between 1 and 20: ");
-            number = keyboard.nextInt();
-        }
+    while (number < 1 || number > 20) {
+  	  System.out.println("Your number is out of range. Please enter a number between 1 and 20: ");
+      number = keyboard.nextInt();
+    }
         
         // Initialize computation component
-        ComputeFactorial calculator = new CalculatorFac();
+    ComputeFactorial calculator = new CalculatorFac();
         
         // Compute and display result
-        long result = calculator.calculateFactorial(number);
-        System.out.println("The factorial of " + number + " is: " + result);
+    long result = calculator.calculateFactorial(number);
+    System.out.println("The factorial of " + number + " is: " + result);
         
-        keyboard.close();
-    }
+    keyboard.close();
+  }
 }
