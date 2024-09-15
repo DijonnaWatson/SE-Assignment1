6b895108a6124dd96b0d8320be54dc0571073a78
DijonnaWatson <167108324+DijonnaWatson@users.noreply.github.com>
2024-09-15 13:26:43 -0400

    Update OutputDestination.java
    fixing indentation again

--- OutputDestination.java
+++ OutputDestination.java
@@ -1,3 +1,3 @@
 public interface OutputDestination {
-	void outputFactors(int[] factors, String outputDestination);  // Output factors (could be file or console)
+    void outputFactors(int[] factors, String outputDestination);  // Output factors (could be file or console)
 }
