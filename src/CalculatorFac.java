b70da2cfe0119121e6785ea53a13f8d6050f2f4c
ViceRaven <167108271+ViceRaven@users.noreply.github.com>
2024-09-15 16:22:42 -0400

    Update CalculatorFac.java

--- src/CalculatorFac.java
+++ src/CalculatorFac.java
@@ -2,15 +2,15 @@ package com.example.factorial;
 
 public class CalculatorFac implements ComputeFactorial {
 
-    @Override
+  @Override
     public long calculateFactorial(int n) {
-        if (n == 0) {
-            return 1;
-        }
-        long result = 1;
-        for (int i = 1; i <= n; i++) {
-            result *= i;
-        }
-        return result;
+    if (n == 0) {
+      return 1;
     }
+    long result = 1;
+    for (int i = 1; i <= n; i++) {
+      result *= i;
+    }
+    return result;
+  }
 }
