d26ca772c30cc81b30088504fb4408cd017c7d84
ViceRaven <167108271+ViceRaven@users.noreply.github.com>
2024-09-22 12:33:13 -0400

    Update InMemoryFactorDataStore.java

--- test/InMemoryFactorDataStore.java
+++ test/InMemoryFactorDataStore.java
@@ -1,19 +1,10 @@
 package com.example.factorfinder;
 import com.example.factorfinder.WriteResult;
 
-// Enum for WriteResult to represent possible outcomes
-enum WriteResult { SUCCESS, FAILURE }
-
 // In-Memory Data Store Implementation
 public class InMemoryFactorDataStore implements FactorDataStore {
   @Override
   public Iterable<Integer> read(InputConfig input) {
     return input.getInputData();
-  }
-
-  @Override
-  public WriteResult appendSingleResult(OutputConfig output, String result) {
-    output.writeOutputData(List.of(result));
-    return WriteResult.SUCCESS;
   }
 }
