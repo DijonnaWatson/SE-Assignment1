c18257979805b7210465a9f4f575506baeab65d6
DijonnaWatson <167108324+DijonnaWatson@users.noreply.github.com>
2024-09-28 13:51:34 -0400

    Update WriteResult.java
    fixed the WriteResult again to match the FactorComputeResult

--- src/factorFinder/WriteResult.java
+++ src/factorFinder/WriteResult.java
@@ -1,10 +1,23 @@
-package com.example.factorfinder;
+package factorfinder;
 
 public interface WriteResult {
+  static WriteResult SUCCESS = () -> WriteResultStatus.SUCCESS;
+  static WriteResult FAILURE = () -> WriteResultStatus.FAILURE;
+
   WriteResultStatus getStatus();
 
   public static enum WriteResultStatus {
-    SUCCESS,
-    FAILURE;
+    SUCCESS(true),
+    FAILURE(false);
+
+    private final boolean success;
+
+    private WriteResultStatus(boolean success) {
+      this.success = success;
+    }
+
+    public boolean isSuccess() {
+      return success;
+    }
   }
 }
