c9b1953a0a3e3825dd59c7092989b4b62ec1d242
DijonnaWatson <167108324+DijonnaWatson@users.noreply.github.com>
2024-09-28 16:59:23 -0400

    Update TestFactorComputeEngine.java
    fixing the imports

--- test/TestFactorComputeEngine.java
+++ test/TestFactorComputeEngine.java
@@ -2,7 +2,9 @@ package factorfinder;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
 
