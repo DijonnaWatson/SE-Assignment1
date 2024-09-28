a0751b48cae4906ed33351bf4146e433b76dd0a7
ViceRaven <167108271+ViceRaven@users.noreply.github.com>
2024-09-28 14:11:34 -0400

    added missing import statements FactorComputeRequest.java

--- src/factorFinder/FactorComputeRequest.java
+++ src/factorFinder/FactorComputeRequest.java
@@ -1,4 +1,7 @@
+
 package com.example.factorfinder;
+import com.example.config.InputConfig;
+import com.example.config.OutputConfig;
 
 public class FactorComputeRequest {
   private static final char DEFAULT_DELIMITER = ';';
