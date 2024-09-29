acf0789ebb36637ba05f97f05e2bb51077ad82b9
DijonnaWatson <167108324+DijonnaWatson@users.noreply.github.com>
2024-09-29 01:09:23 -0400

    Add files via upload

---
+++ src/factorFinder/FactorComputeResultImpl.java
@@ -0,0 +1,9 @@
+package factorfinder;
+
+public class FactorComputeResultImpl implements FactorComputeResult{
+
+	public ComputeResultStatus getStatus() {
+		return FactorComputeResult.SUCCESS.getStatus();
+	}
+
+}
