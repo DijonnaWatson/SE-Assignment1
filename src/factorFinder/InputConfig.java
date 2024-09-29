de829ce58333dc64d64a4ce2f8dd68f21c3322bf
ViceRaven <167108271+ViceRaven@users.noreply.github.com>
2024-09-28 20:43:59 -0400

    Update InputConfig.java
    added missing override

--- src/factorFinder/InputConfig.java
+++ src/factorFinder/InputConfig.java
@@ -2,6 +2,7 @@ package com.example.factorfinder;
 import java.util.List;
 
 public interface InputConfig {
+  @Override
   List<Integer> getInputData();
   String getFilePath(); // Method to get the file path for input data
 }
