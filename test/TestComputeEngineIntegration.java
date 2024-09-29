d4a30b16112cf2b1dd1806494b7ba59bf1ff8360
ViceRaven <167108271+ViceRaven@users.noreply.github.com>
2024-09-28 23:45:18 -0400

    Update TestComputeEngineIntegration.java
    added missing import

--- test/TestComputeEngineIntegration.java
+++ test/TestComputeEngineIntegration.java
@@ -9,6 +9,7 @@ import com.example.factorfinder.FactorDataStoreImpl;
 import com.example.factorfinder.FileOutputConfig;
 import java.util.Arrays; 
 import java.util.List; 
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
