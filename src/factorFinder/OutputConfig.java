b797a69c876bbeeb9e62a98794197ceb183a4b5e
DijonnaWatson <167108324+DijonnaWatson@users.noreply.github.com>
2024-09-28 22:25:40 -0400

    Update OutputConfig.java
    fixing up the methods, and got rid of an extra method we didn't need

--- src/factorFinder/OutputConfig.java
+++ src/factorFinder/OutputConfig.java
@@ -3,6 +3,5 @@ import java.util.List;
 
 public interface OutputConfig {
   void writeOutputData(List<String> data);
-  List<String> getOutputData();
   String getFilePath(); // Method to get the file path for output data
 }
