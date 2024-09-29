865fa6914e0baf583296561647db69c6a5bfb300
ViceRaven <167108271+ViceRaven@users.noreply.github.com>
2024-09-29 00:22:55 -0400

    Update TestFactorComputationCordinator.java
    attempted to satisfy gradle by adding missing import statement for the computer result interface

--- test/TestFactorComputationCordinator.java
+++ test/TestFactorComputationCordinator.java
@@ -1,7 +1,7 @@
 package com.example.factorfinder;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-
+import com.example.FactorComputeResult;
 import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.Test;
