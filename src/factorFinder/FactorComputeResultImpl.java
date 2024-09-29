5fe4996a1eb2ac0355d87fae62fef99009689df5
DijonnaWatson <167108324+DijonnaWatson@users.noreply.github.com>
2024-09-29 01:25:46 -0400

    Delete src/factorFinder/FactorComputeResultImpl.java
    nevermind we don't need this, i checked and it still works without this implementation, it was the fact that we weren't using the mock request we made and put it into the prototype as a parameter, to then use the compute method and see if the getStatus method worked. I think that's why we were getting a null pointer

--- src/factorFinder/FactorComputeResultImpl.java
+++
@@ -1,7 +0,0 @@
-package com.example.factorfinder;
-
-public class FactorComputeResultImpl implements FactorComputeResult {
-  public ComputeResultStatus getStatus() {
-    return FactorComputeResult.SUCCESS.getStatus();
-  }
-}
