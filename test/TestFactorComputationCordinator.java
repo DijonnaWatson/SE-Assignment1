2f80841ef0dfeefee3d1dca2f5e528012ff3ceb2
ViceRaven <167108271+ViceRaven@users.noreply.github.com>
2024-09-28 20:47:28 -0400

    Update TestFactorComputationCordinator.java
    removed override

--- test/TestFactorComputationCordinator.java
+++ test/TestFactorComputationCordinator.java
@@ -48,10 +48,10 @@ public class TestFactorComputationCordinator {
     //  csv file with integers, a database table with integers, etc - that's
     //  going to be an implementation detail
     InputConfig inputConfig = new InputConfig() {
-      @Override
+     
       public String getFilePath() {
         // Provide a valid implementation
-        return "path/to/input/file";
+        return ""; //return empty string
       }
     };
 
