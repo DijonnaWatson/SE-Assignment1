863e02536ebbbc0db99dda6de41384f17d48e38f
DijonnaWatson <167108324+DijonnaWatson@users.noreply.github.com>
2024-09-29 10:37:11 -0400

    Update InMemoryFactorDataStore.java
    correcting the instances of the FileInputConfig and FileOutputConfig

--- test/InMemoryFactorDataStore.java
+++ test/InMemoryFactorDataStore.java
@@ -8,13 +8,13 @@ public class InMemoryFactorDataStore implements FactorDataStore {
   private List<String> storedResults =
       new ArrayList<>(); // Declare and initialize storedResults
   @Override
-  public Iterable<Integer> read(InputConfig input) {
+  public Iterable<Integer> read(FileInputConfig input) {
     return input.getInputData();
   }
 
   @Override
   public WriteResult appendSingleResult(
-      OutputConfig output, List<String> results) {
+      FileOutputConfig output, List<String> results) {
     // In-memory store simulation: append results to the list
     storedResults.addAll(results);
 
