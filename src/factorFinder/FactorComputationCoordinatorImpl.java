8accecc558e8190ab32b50ea77e2d0194ed1d3c6
ViceRaven <167108271+ViceRaven@users.noreply.github.com>
2024-10-05 17:17:27 -0400

    added try catch to FactorComputationCoordinatorImpl.java

--- src/factorFinder/FactorComputationCoordinatorImpl.java
+++ src/factorFinder/FactorComputationCoordinatorImpl.java
@@ -16,50 +16,56 @@ public class FactorComputationCoordinatorImpl
 
   @Override
   public FactorComputeResult compute(FactorComputeRequest request) {
-    // validating the inputConfig of request
-    if (request.getInputConfig() != null) {
-      // Correlates to step 2a. Here we receive requests from the user to start
-      // the computation
-      FileInputConfig inputConfig = request.getInputConfig();
+    try {
+      // validating the inputConfig of request
+      if (request.getInputConfig() != null) {
+        // Correlates to step 2a. Here we receive requests from the user to
+        // start the computation
+        FileInputConfig inputConfig = request.getInputConfig();
 
-      // Correlates to step 2b. Request that the data storage component read
-      // integers from specified location
-      List<Integer> inputData = dataStore.read(inputConfig);
+        // Correlates to step 2b. Request that the data storage component read
+        // integers from specified location
+        List<Integer> inputData = dataStore.read(inputConfig);
 
-      // another check to make sure the filepath isn't empty
-      if (inputData.isEmpty()) {
-        return FactorComputeResult
-            .FAILURE; // If statement handles case where no data is available
-      }
-
-      // Correlates to step 2c. Passes the integers to the compute component
-      for (Integer number : inputData) {
-        List<String> formattedFactors = computeEngine.getFormattedFactors(
-            number); // Compute the passed integer's factors
-
-        // Correlates to step 2d. Request that the data storage component write
-        // whatever result to the output
-        FileOutputConfig outputConfig = request.getOutputConfig();
-
-        // validating the outputConfig of the request
-        if (outputConfig != null) {
-          WriteResult writeResult = dataStore.appendSingleResult(outputConfig,
-              formattedFactors); // Write formatted factors to output
-
-          if (writeResult.getStatus()
-              != WriteResult.WriteResultStatus.SUCCESS) {
-            return FactorComputeResult.FAILURE; // Handles if writing fails
-          }
-        } else {
-          System.out.print("Output File path is null!");
+        // another check to make sure the filepath isn't empty
+        if (inputData.isEmpty()) {
+          return FactorComputeResult
+              .FAILURE; // If statement handles case where no data is available
         }
-      }
 
-      return FactorComputeResult.SUCCESS; // Both the input and output file
-                                          // paths were correct and existed
-    } else {
-      System.out.print("Input File path is null!");
-      return FactorComputeResult.FAILURE; // so inputCOnfig was null
+        // Correlates to step 2c. Passes the integers to the compute component
+        for (Integer number : inputData) {
+          List<String> formattedFactors = computeEngine.getFormattedFactors(
+              number); // Compute the passed integer's factors
+
+          // Correlates to step 2d. Request that the data storage component
+          // write whatever result to the output
+          FileOutputConfig outputConfig = request.getOutputConfig();
+
+          // validating the outputConfig of the request
+          if (outputConfig != null) {
+            WriteResult writeResult = dataStore.appendSingleResult(outputConfig,
+                formattedFactors); // Write formatted factors to output
+
+            if (writeResult.getStatus()
+                != WriteResult.WriteResultStatus.SUCCESS) {
+              return FactorComputeResult.FAILURE; // Handles if writing fails
+            }
+          } else {
+            System.out.print("Output File path is null!");
+          }
+        }
+
+        return FactorComputeResult.SUCCESS; // Both the input and output file
+                                            // paths were correct and existed
+      }
+      catch (Exception e) {
+      }
+      else {
+        e.printStackTrace();
+        System.out.print("Input File path is null!");
+        return FactorComputeResult.FAILURE; // so inputCOnfig was null
+      }
     }
   }
 }
