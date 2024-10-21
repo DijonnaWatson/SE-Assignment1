10bc5d09f5363498dc9071b21683b5a6cc6d30a1
DijonnaWatson <167108324+DijonnaWatson@users.noreply.github.com>
2024-10-21 19:17:53 -0400

    Add files via upload
    Adding the thread Test files from step 1 and the testInputFile.txt that has some input number in it.

---
+++ test/TestUser.java
@@ -0,0 +1,26 @@
+package factorfinder;
+
+import java.io.File;
+
+
+public class TestUser {
+	
+	private final FactorComputationCordinatorImpl coordinator;
+
+	public TestUser(FactorComputationCordinatorImpl coordinator) {
+		this.coordinator = coordinator;
+	}
+
+	public void run(String outputPath) {
+		char delimiter = ';';
+		String inputPath = "test" + File.separatorChar + "testInputFile.txt";
+		
+		FileInputConfig inputFile =  new FileInputConfig(inputPath);//need the inputFile to be type FileInputConfig
+		FileOutputConfig outputFile = new FileOutputConfig(outputPath);//need the inputFile to be type FileOutputConfig
+		
+		//Created a request from the inputFile,outputFile, and delimiter, to than use the compute method in FactorComputeEngineImpl
+		FactorComputeRequest request = new FactorComputeRequest(inputFile,outputFile,delimiter);
+		coordinator.compute(request);//now I can call the compute method on the coordinator
+	}
+
+}
