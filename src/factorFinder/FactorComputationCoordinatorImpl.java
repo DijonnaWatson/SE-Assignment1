39e43b3d2f000d6eba677b8024d269c5433caf62
jeremy23newman <jeremy23newman@gmail.com>
2024-10-26 13:48:44 -0400

    missing import

--- src/factorFinder/FactorComputationCoordinatorImpl.java
+++ src/factorFinder/FactorComputationCoordinatorImpl.java
@@ -1,6 +1,7 @@
 package com.example.factorfinder;
 
 import java.util.List;
+import java.util.ArrayList;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
