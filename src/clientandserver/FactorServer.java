959aecb3c48743bde847871171eafbe7da4605dd
DijonnaWatson <167108324+DijonnaWatson@users.noreply.github.com>
2024-11-04 16:24:37 -0500

    Update FactorServer.java
    fixing the indentations

--- src/clientandserver/FactorServer.java
+++ src/clientandserver/FactorServer.java
@@ -1,10 +1,7 @@
 package com.example.clientandserver;
 
 public class FactorServer {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+  }
 }
