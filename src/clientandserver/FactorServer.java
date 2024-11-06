7ba6159f1cfc461135f4ac8439b836d03eb61686
DijonnaWatson <167108324+DijonnaWatson@users.noreply.github.com>
2024-11-06 15:21:10 -0500

    Update FactorServer.java
    updating what I have so far for the FactorServer.
    
    I know the check will fail since idk how to get the auto generated files in git and changing the gradle.yml didn't do anything

--- src/clientandserver/FactorServer.java
+++ src/clientandserver/FactorServer.java
@@ -1,7 +1,61 @@
 package com.example.clientandserver;
 
+
+import io.grpc.Grpc;
+import io.grpc.InsecureServerCredentials;
+import io.grpc.Server;
+import io.grpc.protobuf.services.ProtoReflectionService;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
 public class FactorServer {
-  public static void main(String[] args) {
-    // TODO Auto-generated method stub
+  private Server server;
+
+  private void start() throws IOException {
+    /* The port on which the server should run */
+    int port = 15000; 
+
+    server =
+        Grpc.newServerBuilderForPort(port, InsecureServerCredentials.create())
+            .addService(new FactorServiceImpl()) 
+                                                 
+            .addService(ProtoReflectionService.newInstance())
+            .build()
+            .start();
+    System.out.println("Server started on port " + port);
+
+    Runtime.getRuntime().addShutdownHook(new Thread() {
+      @Override
+      public void run() {
+        System.err.println(
+            "*** shutting down gRPC server since JVM is shutting down");
+        try {
+          if (server != null) {
+            server.shutdown().awaitTermination(30, TimeUnit.SECONDS);
+          }
+        } catch (InterruptedException e) {
+          e.printStackTrace(System.err);
+        }
+        System.err.println("*** server shut down");
+      }
+    });
+  }
+
+  /**
+   * Await termination on the main thread since the grpc library uses daemon
+   * threads.
+   */
+  private void blockUntilShutdown() throws InterruptedException {
+    if (server != null) {
+      server.awaitTermination();
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    FactorServer server =
+        new FactorServer(); 
+    server.start();
+    server.blockUntilShutdown();
   }
 }
+
