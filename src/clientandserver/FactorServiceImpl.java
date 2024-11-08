6013151e79f9e5d17cb427ac9e4622b171aaba14
DijonnaWatson <167108324+DijonnaWatson@users.noreply.github.com>
2024-11-07 21:42:13 -0500

    Update FactorServiceImpl.java
    just fixing the formatting

--- src/clientandserver/FactorServiceImpl.java
+++ src/clientandserver/FactorServiceImpl.java
@@ -3,33 +3,40 @@ package clientandserver;
 import apiProto.CoordinatorEngine.coordinatorResponse;
 import apiProto.CoordinatorEngine.coordinatorResponse.FactorComputeResult;
 import apiProto.FactorServiceGrpc.FactorServiceImplBase;
-//import clientandserver.FactorComputeRequest;
+// import clientandserver.FactorComputeRequest;
 
-public class FactorServiceImpl extends FactorServiceImplBase{
-	FactorComputationCordinatorImpl coordinator;
-	
-	public void compute(apiProto.CoordinatorEngine.coordinatorRequest request,
-			io.grpc.stub.StreamObserver<apiProto.CoordinatorEngine.coordinatorResponse> responseObserver) {
-		
-		// create a request from API 1 and save it
-		FileInputConfig internallInputFile = new FileInputConfig(request.getInputFile());
-		FileOutputConfig internalOutputFile = new FileOutputConfig(request.getOutputFile());
-		char internalDelimier = request.getDelimiter().charAt(0);
-		
-		FactorComputeRequest internalRequest = new FactorComputeRequest(internallInputFile, internalOutputFile, internalDelimier);
-		
-		//Save result from request into the internal type
-		FactorComputeResult internalResponse = coordinator.compute(internalRequest);//TODO : make type FactorComputeRequest and get Result but need to make it back into the coordinatorRepsonse
-		
-		//Translate it back into external type 
-		coordinatorResponse	externalResponse = new coordinatorResponse(internalResponse);
-		responseObserver.onNext(externalResponse);//sending the byte back to the Client on the open socket
-		responseObserver.onCompleted();
-	}
-		
-	public FactorServiceImpl(FactorComputeEngineImpl computeEngine, FactorDataStoreImpl dataStore) {
+public class FactorServiceImpl extends FactorServiceImplBase {
+  FactorComputationCordinatorImpl coordinator;
 
-		coordinator = new FactorComputationCordinatorImpl(computeEngine,dataStore);
-	}
+  public void compute(apiProto.CoordinatorEngine.coordinatorRequest request,
+      io.grpc.stub.StreamObserver<
+          apiProto.CoordinatorEngine.coordinatorResponse> responseObserver) {
+    // create a request from API 1 and save it
+    FileInputConfig internallInputFile =
+        new FileInputConfig(request.getInputFile());
+    FileOutputConfig internalOutputFile =
+        new FileOutputConfig(request.getOutputFile());
+    char internalDelimier = request.getDelimiter().charAt(0);
 
+    FactorComputeRequest internalRequest = new FactorComputeRequest(
+        internallInputFile, internalOutputFile, internalDelimier);
+
+    // Save result from request into the internal type
+    FactorComputeResult internalResponse = coordinator.compute(
+        internalRequest); // TODO : make type FactorComputeRequest and get
+                          // Result but need to make it back into the
+                          // coordinatorRepsonse
+
+    // Translate it back into external type
+    coordinatorResponse externalResponse =
+        new coordinatorResponse(internalResponse);
+    responseObserver.onNext(externalResponse); // sending the byte back to the
+                                               // Client on the open socket
+    responseObserver.onCompleted();
+  }
+
+  public FactorServiceImpl(
+      FactorComputeEngineImpl computeEngine, FactorDataStoreImpl dataStore) {
+    coordinator = new FactorComputationCordinatorImpl(computeEngine, dataStore);
+  }
 }
